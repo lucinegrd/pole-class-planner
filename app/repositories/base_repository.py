292f28a7e10129edf784350aa3d64502efff6d2b
lucine <lucine.giraud@telecomnancy.eu>
2025-03-22 18:45:27 -0400

    feat : ajout de la classe BaseRepository comme modèle de repository

---
+++ app/repositories/base_repository.py
@@ -0,0 +1,26 @@
+from app import db
+
+class BaseRepository:
+
+    model = None
+
+    @classmethod
+    def get_all(cls):
+        try:
+            return cls.model.query.order_by(cls.model.name).all()
+        except AttributeError:
+            return cls.model.query.all()
+
+    @classmethod
+    def get_by_id(cls, obj_id: int):
+        return cls.model.query.get(obj_id)
+
+    @classmethod
+    def create(cls, obj):
+        db.session.add(obj)
+        db.session.commit()
+
+    @classmethod
+    def delete(cls, obj):
+        db.session.delete(obj)
+        db.session.commit()
