1afbeffb62c7d23b68e9a08cbebf3b63a1d1c5a9
lucine <lucine.giraud@telecomnancy.eu>
2025-03-22 19:54:56 -0400

    fix : prise en compte des tests pour app

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,23 @@
 from flask import Flask
-from app.routes import register_routes
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(config_name=None):
     app = Flask(__name__, template_folder="templates")
-    app.config.from_object("config.Config")
+
+    # Config dynamique
+    if config_name == "testing":
+        app.config.from_object("config.TestingConfig")
+    else:
+        app.config.from_object("config.DevelopmentConfig")
 
     db.init_app(app)
     with app.app_context():
         db.create_all()
         print("Base de données initialisée avec succès !")
 
+    from app.routes import register_routes
     register_routes(app)
 
     return app
