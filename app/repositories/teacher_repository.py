27d856850a7b2ddf957a19e00bd21c8fe8617943
lucine <lucine.giraud@telecomnancy.eu>
2025-03-30 20:37:18 -0400

    feat : ajout d'une fonction edit des teachers

--- app/repositories/teacher_repository.py
+++ app/repositories/teacher_repository.py
@@ -10,3 +10,13 @@ class TeacherRepository(BaseRepository):
     @staticmethod
     def get_by_email(email: str):
         return Teacher.query.filter_by(email=email).first()
+
+    @staticmethod
+    def edit_teacher(teacher: Teacher, first_name="", last_name="", email=""):
+        if first_name != "" :
+            teacher.first_name = first_name
+        if last_name != "" :
+            teacher.last_name = last_name
+        if email != "":
+            teacher.email = email
+        db.session.commit()
