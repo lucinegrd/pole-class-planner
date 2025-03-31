a9f7f766a14856fc21cb0e5af3ff9fec0029124e
lucine <lucine.giraud@telecomnancy.eu>
2025-03-30 20:37:54 -0400

    feat : importation des nouvelles routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,11 +1,20 @@
 from flask import Blueprint
 
-from app.routes.courses import courses_bp
-from app.routes.students import students_bp
-from app.routes.main import main_bp
+from app.routes.courses_routes import courses_bp
+from app.routes.courses_types_routes import course_type_bp
+from app.routes.levels_routes import level_bp
+from app.routes.rooms_routes import room_bp
+from app.routes.students_routes import students_bp
+from app.routes.main_routes import main_bp
+from app.routes.teachers_routes import teachers_bp
+
 
 def register_routes(app):
     """Enregistre toutes les routes de l'application."""
     app.register_blueprint(students_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(courses_bp)
+    app.register_blueprint(teachers_bp)
+    app.register_blueprint(course_type_bp)
+    app.register_blueprint(room_bp)
+    app.register_blueprint(level_bp)
