4685ec86970dcade833ea6df5133866301b4bfc7
lucine <lucine.giraud@telecomnancy.eu>
2025-04-15 19:52:19 -0400

    feat : utilisation de LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
 
 
 db = SQLAlchemy()
@@ -14,6 +18,8 @@ def create_app(config_name=None):
         app.config.from_object("config.DevelopmentConfig")
 
     db.init_app(app)
+    login_manager.init_app(app)
+
     with app.app_context():
         from app.models import Course, CourseType, Level, Room, Teacher, Student
         db.create_all()
@@ -23,3 +29,8 @@ def create_app(config_name=None):
     register_routes(app)
 
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import Teacher
+    return db.session.get(Teacher, int(user_id))
