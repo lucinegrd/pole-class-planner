c44d5d543d76dd6e35c161aad92cc22e59a6bd9f
lucine <lucine.giraud@telecomnancy.eu>
2025-04-17 01:17:44 -0400

    refacto : modèle Course amélioré

--- app/models/course.py
+++ app/models/course.py
@@ -1,6 +1,7 @@
 from app import db
-from .association_tables import registration, waiting_list
 from app.models import Student, Teacher, CourseType, Room, Level
+from .registration import Registration
+
 
 class Course(db.Model):
     __tablename__ = 'course'
@@ -12,19 +13,26 @@ class Course(db.Model):
     date = db.Column(db.DateTime, nullable=False)
     id_room = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
     id_course_type = db.Column(db.Integer, db.ForeignKey('course_type.id'), nullable=False)
+    canceled = db.Column(db.Boolean, nullable=False, default=False)
 
     # Relations
     room = db.relationship(Room, backref='courses', lazy="select")
     level = db.relationship(Level, backref='courses', lazy="select")
     teacher = db.relationship(Teacher, backref="courses", lazy="select")
     course_type = db.relationship(CourseType, backref="courses", lazy="select")
-    students = db.relationship(Student, secondary=registration, backref="courses", lazy="select")
-    waiting_list = db.relationship(Student, secondary=waiting_list, backref="waiting_courses", lazy="select")
 
     @property
     def is_full(self):
-        return len(self.students) >= self.course_type.places
+        return len(self.students_confirmed) >= self.course_type.places
+
+    @property
+    def students_confirmed(self):
+        return [reg.student for reg in self.registrations if reg.state != "Annulé" and reg.state != "Sur liste d\'attente"]
 
     @property
     def places(self):
-        return self.course_type.places - len(self.students)
+        return self.course_type.places - len(self.students_confirmed)
+
+    @property
+    def students(self):
+        return [reg.student for reg in self.registrations]
